5a0159d505ad49881d805a15a6f41096e68213f7
Berke Tınas <berke_750@hotmail.com>
2022-12-19 17:06:04 +0300

    cors allow all origins, get mappings -> post mappings

--- payroll/src/main/java/com/ember/payroll/PayrollApplication.java
+++ payroll/src/main/java/com/ember/payroll/PayrollApplication.java
@@ -2,9 +2,16 @@ package com.ember.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PayrollApplication {
+public class PayrollApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PayrollApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
--- payroll/src/main/java/com/ember/payroll/controller/SalaryController.java
+++ payroll/src/main/java/com/ember/payroll/controller/SalaryController.java
@@ -3,15 +3,14 @@ package com.ember.payroll.controller;
 import com.ember.payroll.model.PayloadDTO;
 import com.ember.payroll.model.ResponseDTO;
 import com.ember.payroll.service.SalaryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
 @RestController
+@CrossOrigin
 public class SalaryController {
     private final SalaryService salaryService;
 
@@ -25,26 +24,26 @@ public class SalaryController {
         salaryService.testProperties();
     }
 
-    @GetMapping("/tr/planned-format")
+    @PostMapping("/tr/planned-format")
     public void plannedFormatTest(@RequestBody PayloadDTO payloadDTO) {
         payloadDTO.getYearlyReport().forEach(System.out::println);
         System.out.println(payloadDTO.getEmployeeType());
     }
 
     // GROSS SALARY TO NET SALARY CONVERSION
-    @GetMapping("/tr/gross-to-net")
+    @PostMapping("/tr/gross-to-net")
     public List<ResponseDTO> trGrossToNet(@RequestBody PayloadDTO payload) {
         return salaryService.trGrossToNet_INNER(payload);
     }
 
     // NET SALARY TO GROSS SALARY CONVERSION
-    @GetMapping("/tr/net-to-gross")
+    @PostMapping("/tr/net-to-gross")
     public List<ResponseDTO> trNetToGross(@RequestBody PayloadDTO payload) {
         return salaryService.trNetToGross_INNER(payload);
     }
 
     // NET SALARY TO EMPLOYER COST CONVERSION
-    @GetMapping("/tr/net-to-cost")
+    @PostMapping("/tr/net-to-cost")
     public List<ResponseDTO> trNetToCost(@RequestBody PayloadDTO payload) {
         List<ResponseDTO> yearlyReport = salaryService.trNetToGross_INNER(payload);
         payload.setYearlyReport(yearlyReport.stream().flatMapToDouble(element -> DoubleStream.of(element.getGross())).boxed().collect(Collectors.toList()));
@@ -52,7 +51,7 @@ public class SalaryController {
     }
 
     // GROSS SALARY TO EMPLOYER COST CONVERSION
-    @GetMapping("/tr/gross-to-cost")
+    @PostMapping("/tr/gross-to-cost")
     public List<ResponseDTO> trGrossToCost(@RequestBody PayloadDTO payload) {
         return salaryService.trGrossToCost_INNER(payload);
     }
